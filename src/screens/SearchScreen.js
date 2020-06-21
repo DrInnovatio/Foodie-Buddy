c0212f68d79eda07e8309b05ae3b3e79c59f9eb3
DrInnovatio <turing1723@gmail.com>
2020-06-21 17:42:00 +1000

    added error handling (try and catch)

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,16 +7,21 @@ const SearchScreen = () => {
 
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'Brisbane'
-      }
-    });
-    setResults(response.data.businesses)
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'Brisbane'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong!");
+    }
   };
 
   return (
@@ -26,7 +31,7 @@ const SearchScreen = () => {
         onTermChange={ setTerm }
         onTermSubmit={ searchApi }
       />
-      <Text>Sreach Screen</Text>
+      { errorMessage ? <Text>{ errorMessage }</Text> : null }
       <Text>We have found { results.length } results.</Text>
     </View>
   )

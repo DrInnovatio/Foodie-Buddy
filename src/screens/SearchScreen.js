9eb2a3c9f3aae7974bb23c94c570bf142a6b011b
DrInnovatio <turing1723@gmail.com>
2020-06-23 14:41:39 +1000

    added useEffect on SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -9,12 +9,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'Brisbane'
         }
       });
@@ -23,13 +23,18 @@ const SearchScreen = () => {
       setErrorMessage("Something went wrong!");
     }
   };
+  // Call searchAPI when component is first rendered. BAD CODE!!
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, [])
 
   return (
     <View>
       <SearchBar
         term={ term }
         onTermChange={ setTerm }
-        onTermSubmit={ searchApi }
+        onTermSubmit={ () => searchApi(term) }
       />
       { errorMessage ? <Text>{ errorMessage }</Text> : null }
       <Text>We have found { results.length } results.</Text>

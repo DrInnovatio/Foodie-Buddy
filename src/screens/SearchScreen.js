b1b8b833d45d0a7fccbad7ca3f57db4db4946dc7
DrInnovatio <turing1723@gmail.com>
2020-06-27 22:33:18 +1000

    empty element and added margin bottom under the search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -17,20 +17,20 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={ { flex: 1 } }>
+    <>
       <SearchBar
         term={ term }
         onTermChange={ setTerm }
         onTermSubmit={ () => searchApi(term) }
       />
       { errorMessage ? <Text>{ errorMessage }</Text> : null }
-      <Text>We have found { results.length } results.</Text>
+
       <ScrollView>
         <ResultsLists results={ filterResultsByPrice('$') } title="Cost Effective" />
         <ResultsLists results={ filterResultsByPrice('$$') } title="Bit Pricier" />
         <ResultsLists results={ filterResultsByPrice('$$$') } title="Big Spender" />
       </ScrollView>
-    </View>
+    </>
   )
 };
 
